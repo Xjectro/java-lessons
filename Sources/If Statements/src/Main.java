99d04fcc65d874982239adc4ef73e9306d8934f7
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-06-01 15:10:39 +0300

    If Statements

---
+++ Sources/If Statements/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is a simple Java program that demonstrates the use of if statements
+        int number = 10;
+
+        // Check if the number is positive, negative, or zero
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
