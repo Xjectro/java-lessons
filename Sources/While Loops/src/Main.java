4f57c3c67509fcfeebdb60a77d46cec5bfa9863b
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-06-01 23:24:17 +0300

    While Loops

---
+++ Sources/While Loops/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isRaining = true;
+
+        while (isRaining) {
+            System.out.println("It's raining, stay inside!");
+            // Simulate a change in weather
+            isRaining = false; // This will end the loop
+        }
+    }
+}
