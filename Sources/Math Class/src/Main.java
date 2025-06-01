5e90f766929e753cfe36cd7be541c90da5fab4f9
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-06-01 15:29:12 +0300

    Math Class

---
+++ Sources/Math Class/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println(Math.PI);
+        // System.out.println(Math.E);
+
+        double result;
+
+        result = Math.pow(2, 3);
+        result = Math.abs(-5);
+        result = Math.sqrt(16);
+        result = Math.max(10, 20);
+        result = Math.min(10, 20);
+        result = Math.random();
+        result = Math.round(3.6);
+
+        System.out.println(result);
+    }
+}
