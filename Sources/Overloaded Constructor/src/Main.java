dd52d2648d90dfb6d877634a6005a48f4e264e89
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-06-02 15:01:39 +0300

    Overloaded Constructor

---
+++ Sources/Overloaded Constructor/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("john_doe");
+
+        System.out.println("Username: " + user1.username);
+        System.out.println("Email: " + user1.email);
+        System.out.println("Age: " + user1.age);
+    }
+}
