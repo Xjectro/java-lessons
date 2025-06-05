35fcc4e362c12e871b23b0165b9215284f42ae44
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-06-05 13:44:56 +0300

    Arraylists

---
+++ Sources/Arraylists/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(3);
+        list.add(1);
+        list.add(4);
+
+        System.out.println("List before sorting: " + list);
+    }
+}
