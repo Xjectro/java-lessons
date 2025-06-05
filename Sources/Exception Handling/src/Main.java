6c4f780c90154712eccf17db080a572b75531c45
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-06-05 13:48:47 +0300

    Exception Handling

---
+++ Sources/Exception Handling/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println(1 / 0);
+        } catch (ArithmeticException e) {
+            System.out.println("Caught an arithmetic exception: " + e.getMessage());
+        } finally {
+            System.out.println("This block always executes.");
+        }
+    }
+}
